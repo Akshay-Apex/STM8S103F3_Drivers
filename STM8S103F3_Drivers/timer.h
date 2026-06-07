2efbf2909a6cc28ec84a8a87f65205f804ee35af
Akshay <akshayknaik777@gmail.com>
2026-06-07 11:15:37 +0530

    Initial commit

--- STM8S103F3_Drivers/timer.h
+++ STM8S103F3_Drivers/timer.h
@@ -44,6 +44,59 @@ typedef struct {
   volatile uint8_t OISR;  // TIM1 output idle state register
 } TIM1_REG;
 
+
+/* TIM2 Register Definitions */
+typedef struct {
+  volatile uint8_t CR1;   // TIM2 control register 1
+  volatile uint8_t Reserved_0; 
+  volatile uint8_t Reserved_1; 
+
+  volatile uint8_t IER;   // TIM2 Interrupt enable register
+  volatile uint8_t SR1;   // TIM2 status register 1
+  volatile uint8_t SR2;   // TIM2 status register 2
+  volatile uint8_t EGR;   // TIM2 event generation register
+
+  volatile uint8_t CCMR1; // TIM2 capture/compare mode register 1
+  volatile uint8_t CCMR2; // TIM2 capture/compare mode register 2
+  volatile uint8_t CCMR3; // TIM2 capture/compare mode register 3
+
+  volatile uint8_t CCER1; // TIM2 capture/compare enable register 1
+  volatile uint8_t CCER2; // TIM2 capture/compare enable register 2
+  volatile uint8_t CNTRH; // TIM2 counter high
+  volatile uint8_t CNTRL; // TIM2 counter low
+  volatile uint8_t PSCR;  // IM2 prescaler register
+  volatile uint8_t ARRH;  // TIM2 auto-reload register high
+  volatile uint8_t ARRL;  // TIM2 auto-reload register low
+
+  volatile uint8_t CCR1H; // TIM2 capture/compare register 1 high
+  volatile uint8_t CCR1L; // TIM2 capture/compare register 1 low
+  volatile uint8_t CCR2H; // TIM2 capture/compare register 2 high
+  volatile uint8_t CCR2L; // TIM2 capture/compare register 2 low
+  volatile uint8_t CCR3H; // TIM2 capture/compare register 3 high
+  volatile uint8_t CCR3L; // TIM2 capture/compare register 3 low
+} TIM2_REG;
+
+
+/* TIM4 Register Definitions */
+typedef struct {
+  volatile uint8_t CR1;         // TIM4 control register 1
+  volatile uint8_t Reserved_0;  
+  volatile uint8_t Reserved_1;
+  volatile uint8_t IER;         // TIM4 interrupt enable register
+  volatile uint8_t SR;          // TIM4 status register
+  volatile uint8_t EGR;         // TIM4 event generation register
+  volatile uint8_t CNTR;        // TIM4 counter
+  volatile uint8_t PSCR;        // TIM4 prescaler register
+  volatile uint8_t ARR;         // TIM4 auto-reload register
+} TIM4_REG;
+
 #define TIM1 ((TIM1_REG *)0x5250) // Base address binding of TIM1 registers
+#define TIM2 ((TIM2_REG *)0x5300) // Base address binding of TIM2 registers
+#define TIM4 ((TIM4_REG *)0x5340) // Base address binding of TIM4 registers
+
+
+
+/* TIM4 Timer Functions */
+
 
 #endif
