d3900f88f21de5d8ba3dd6b1b22ede78f190ff60
Akshay <akshayknaik777@gmail.com>
2026-06-16 17:32:41 +0530

    Partial commit

--- STM8S103F3_Drivers/timer.h
+++ STM8S103F3_Drivers/timer.h
@@ -94,7 +94,55 @@ typedef struct {
 #define TIM2 ((TIM2_REG *)0x5300) // Base address binding of TIM2 registers
 #define TIM4 ((TIM4_REG *)0x5340) // Base address binding of TIM4 registers
 
+typedef enum {
+  TIM2_CC1_OUTPUT    = 0x00,
+  TIM2_CC1_INPUT_TI1 = 0x01,
+  TIM2_CC1_INPUT_TI2 = 0x02  
+} TIM2_CC1_Channel_Mode;
 
+#define TIM2_CC1_CHANNEL_MODE_CLR_MASK 0xFC
+
+typedef enum {
+    TIM2_OC1_FROZEN            = 0x00,
+    TIM2_OC1_ACTIVE_ON_MATCH   = 0x01,
+    TIM2_OC1_INACTIVE_ON_MATCH = 0x02,
+    TIM2_OC1_TOGGLE            = 0x03,
+    TIM2_OC1_FORCE_INACTIVE    = 0x04,
+    TIM2_OC1_FORCE_ACTIVE      = 0x05,
+    TIM2_OC1_PWM_MODE_1        = 0x06,
+    TIM2_OC1_PWM_MODE_2        = 0x07
+} TIM2_OUTPUT_COMPARE1_MODE;
+
+typedef enum {
+    TIM2_IC1_PRESCALER_1 = 0x00,  // Capture every event
+    TIM2_IC1_PRESCALER_2 = 0x01,  // Capture every 2 events
+    TIM2_IC1_PRESCALER_4 = 0x02,  // Capture every 4 events
+    TIM2_IC1_PRESCALER_8 = 0x03   // Capture every 8 events
+} TIM2_INPUT_CAPTURE1_PRESCALER;
+
+typedef enum {
+    TIM2_IC1_FILTER_NONE    = 0x00,
+    TIM2_IC1_FILTER_FM_N2   = 0x01,
+    TIM2_IC1_FILTER_FM_N4   = 0x02,
+    TIM2_IC1_FILTER_FM_N8   = 0x03,
+
+    TIM2_IC1_FILTER_FM2_N6  = 0x04,
+    TIM2_IC1_FILTER_FM2_N8  = 0x05,
+
+    TIM2_IC1_FILTER_FM4_N6  = 0x06,
+    TIM2_IC1_FILTER_FM4_N8  = 0x07,
+
+    TIM2_IC1_FILTER_FM8_N6  = 0x08,
+    TIM2_IC1_FILTER_FM8_N8  = 0x09,
+
+    TIM2_IC1_FILTER_FM16_N5 = 0x0A,
+    TIM2_IC1_FILTER_FM16_N6 = 0x0B,
+    TIM2_IC1_FILTER_FM16_N8 = 0x0C,
+
+    TIM2_IC1_FILTER_FM32_N5 = 0x0D,
+    TIM2_IC1_FILTER_FM32_N6 = 0x0E,
+    TIM2_IC1_FILTER_FM32_N8 = 0x0F
+} TIM2_INPUT_CAPTURE1_FILTER;
 
 /* TIM4 Timer Functions */
 /* TIM4 control register 1 (CR1) */
@@ -324,6 +372,87 @@ static inline uint8_t tim2_capture_compare1_overcapture_flag_read(void) {
   return ((TIM2->SR2 >> 1) & 1);
 }
 
+static inline void tim2_capture_compare2_overcapture_flag_clear(void) {
+  TIM2->SR2 &= ~(1U << 2);
+}
+
+static inline uint8_t tim2_capture_compare2_overcapture_flag_read(void) {
+  return ((TIM2->SR2 >> 2) & 1);
+}
+
+static inline void tim2_capture_compare3_overcapture_flag_clear(void) {
+  TIM2->SR2 &= ~(1U << 3);
+}
+
+static inline uint8_t tim2_capture_compare3_overcapture_flag_read(void) {
+  return ((TIM2->SR2 >> 3) & 1);
+}
+
+
+
+/* TIM2 event generation register (EGR) */
+static inline void tim2_update_event_generate(void) {
+  TIM2->EGR |= (1U << 0);
+}
+
+static inline void tim2_capture_compare1_event_generate(void) {
+  TIM2->EGR |= (1U << 1);
+}
+
+static inline void tim2_capture_compare2_event_generate(void) {
+  TIM2->EGR |= (1U << 2);
+}
+
+static inline void tim2_capture_compare3_event_generate(void) {
+  TIM2->EGR |= (1U << 3);
+}
+
+
+
+/* TIM2 Capture/Compare mode register 1 (CCMR1) */
+/* Generic Functions */
+static inline void tim2_capture_compare1_channel_mode_set(TIM2_CC1_Channel_Mode mode) {
+  TIM2->CCMR1 = (TIM2->CCMR1 & TIM2_CC1_CHANNEL_MODE_CLR_MASK) | mode;
+}
+
+static inline TIM2_CC1_Channel_Mode tim2_capture_compare1_channel_mode_read(void) {
+  return (TIM2_CC1_Channel_Mode)(TIM2->CCMR1 & ~TIM2_CC1_CHANNEL_MODE_CLR_MASK);
+}
+
+/* Output mode functions */
+static inline void tim2_capture_compare1_preload_enable(void) {
+  TIM2->CCMR1 |= (1U << 3);
+}
+
+static inline void tim2_capture_compare1_preload_disable(void) {
+  TIM2->CCMR1 &= ~(1U << 3);
+}
+
+static inline void tim2_output_compare1_mode_set(TIM2_OUTPUT_COMPARE1_MODE mode) {
+  TIM2->CCMR1 = (TIM2->CCMR1 & 0x8F) | ((uint8_t)mode << 4);
+}
+
+static inline TIM2_OUTPUT_COMPARE1_MODE tim2_output_compare1_mode_read(void) {
+  return (TIM2_OUTPUT_COMPARE1_MODE)((TIM2->CCMR1 >> 4) & 0x07);
+}
+
+/* Input mode functions */
+
+static inline void tim2_input_capture1_prescaler_set(TIM2_INPUT_CAPTURE1_PRESCALER psc) {
+  TIM2->CCMR1 = (TIM2->CCMR1 & 0xF3) | ((uint8_t)psc << 2);
+}
+
+static inline TIM2_INPUT_CAPTURE1_PRESCALER tim2_input_capture1_prescaler_read(void) {
+  return (TIM2_INPUT_CAPTURE1_PRESCALER)((TIM2->CCMR1 >> 2) & 0x03));
+}
+
+static inline void tim2_input_capture1_filter_set(TIM2_INPUT_CAPTURE1_FILTER filter) {
+  TIM2->CCMR1 = (TIM2->CCMR1 & 0x0F) | ((uint8_t)filter << 4);
+}
+
+static inline TIM2_INPUT_CAPTURE1_FILTER tim2_input_capture1_filter_read(void) {
+  return (TIM2_INPUT_CAPTURE1_FILTER)(TIM2->CCMR1 >> 4) & 0x0F;
+}
 
 
 #endif
