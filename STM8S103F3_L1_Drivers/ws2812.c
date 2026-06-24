b86057ffff6b6c9d0c7666f9c9254e3e02125fac
Akshay <akshayknaik777@gmail.com>
2026-06-24 23:40:12 +0530

    WS2812 driver updated init function

--- STM8S103F3_L1_Drivers/ws2812.c
+++ STM8S103F3_L1_Drivers/ws2812.c
@@ -3,6 +3,7 @@
 #include "../STM8S103F3_L0_Drivers/timer.h"
 #include "./ws2812.h"
 
+#define WS2812_TIM2_PSC 0
 
 /*=============================================================*
  * 
@@ -11,15 +12,49 @@
  *=============================================================*/
 
 /* Definition of global WS2812 service variables (shared via extern in ws2812.h) */
-
+uint16_t TIM2_ARR = 0;
+uint16_t TIM2_COMPARE_BIT_0 = 0;
+uint16_t TIM2_COMPARE_BIT_1 = 0;
 
 /* Timing Parameters Calibration Function */
 void ws2812_timing_calibrate(void) {
-
+  uint8_t fmaster_freq_mhz = (clk_fmaster_freq_khz_get() >> 10) + 1;
+  if(fmaster_freq_mhz == 16) {    
+    TIM2_ARR = 20;
+    TIM2_COMPARE_BIT_0 = 6;
+    TIM2_COMPARE_BIT_1 = 12;
+  } else if(fmaster_freq_mhz == 8) {    
+    TIM2_ARR = 10;
+    TIM2_COMPARE_BIT_0 = 3;
+    TIM2_COMPARE_BIT_1 = 6;
+  } else if(fmaster_freq_mhz == 4) {    
+    TIM2_ARR = 5;
+    TIM2_COMPARE_BIT_0 = 1;
+    TIM2_COMPARE_BIT_1 = 3;
+  } 
 }
 
 
-void ws2812_init(GPIO_PORT_REG *port, uint8_t pin) {  
-  gpio_out_push_pull_fast_mode(port, pin);
-  
+// NOt completed =========   ===  ===========  ===== =    = = =
+void ws2812_init(uint8_t pin) {    
+  clk_peripheral_1_clock_enable(CLK_TIM2);
+  tim2_prescaler_set(0);  
+
+  if(pin == 4) {    
+    tim2_cc1_channel_mode_set(TIM2_CC1_OUTPUT);
+    tim2_cc1_enable();
+
+  } else if(pin == 3) {    
+    tim2_cc2_channel_mode_set(TIM2_CC2_OUTPUT);
+    tim2_cc2_enable();
+
+  } else if(pin == 2) {    
+    tim2_cc3_channel_mode_set(TIM2_CC3_OUTPUT);
+    tim2_cc3_enable();
+  }
+
+  gpio_out_push_pull_fast_mode(GPIO_D, pin);
 }
+
+
+void ws2812_deinit(void);
--- STM8S103F3_L1_Drivers/ws2812.h
+++ STM8S103F3_L1_Drivers/ws2812.h
@@ -3,9 +3,6 @@
 
 #include <stdint.h>
 
-#define WS2812_T0H 6
-#define WS2812_T1H 12
-
 /*=============================================================*
 * 
 * WS2812 Public API Declarations BEGIN 
@@ -13,6 +10,10 @@
 *=============================================================*/
 
 /* Global Variable and Constant Declarations */
+extern uint16_t TIM2_PSC;
+extern uint16_t TIM2_ARR;
+extern uint16_t TIM2_COMPARE_BIT_0;
+extern uint16_t TIM2_COMPARE_BIT_1;
 
 
 /* Timing Parameters Calibration Function */
@@ -20,20 +21,7 @@ void ws2812_timing_calibrate(void);
 
 void ws2812_init(uint8_t pin);
 
-typedef struct {
-  uint8_t green;
-  uint8_t red;
-  uint8_t blue;
-} ws2812_pixel_t;
-
-void ws2812_set_pixel(uint8_t index, ws2812_pixel_t pixel);
-
-void ws2812_clear(void);
-
-void ws2812_show(void);
-
-void ws2812_reset(void);
-
+void ws2812_deinit(void);
 /*=============================================================*
  * WS2812 Public API Declarations END
  *=============================================================*/
