5b6e5c3182f9f81a2a566eed2ccd3050e49db9b7
Akshay <akshayknaik777@gmail.com>
2026-06-30 13:45:18 +0530

    WS2812 Driver finished

--- STM8S103F3_L1_Drivers/ws2812.c
+++ STM8S103F3_L1_Drivers/ws2812.c
@@ -4,40 +4,25 @@
 #include "../STM8S103F3_L1_Drivers/time.h"
 #include "./ws2812.h"
 
-#define WS2812_TIM2_PSC 0
 
 /*=============================================================*
 * 
 * WS2812 Public API Definitions
 *
 *=============================================================*/
-
-/* Definition of global WS2812 service variables (shared via extern in ws2812.h) */
-uint8_t TIM2_ARR = 0;
-uint8_t TIM2_COMPARE_BIT_0 = 0;
-uint8_t TIM2_COMPARE_BIT_1 = 0;
+/* Interrupt Enable and Disable */
+#define global_interrupt_enable()  __asm__("rim") 
+#define global_interrupt_disable() __asm__("sim")
 
 
-/* Timing Parameters Calibration Function */
-void ws2812_timing_calibrate(void) {
-  uint8_t fmaster_freq_mhz = (clk_fmaster_freq_khz_get() >> 10) + 1;
-  if(fmaster_freq_mhz == 16) {    
-    TIM2_ARR = 19U;
-    TIM2_COMPARE_BIT_0 = 6U;
-    TIM2_COMPARE_BIT_1 = 12U;
-  } else if(fmaster_freq_mhz == 8) {    
-    TIM2_ARR = 9U;
-    TIM2_COMPARE_BIT_0 = 3U;
-    TIM2_COMPARE_BIT_1 = 6U;
-  } else if(fmaster_freq_mhz == 4) {    
-    TIM2_ARR = 4U;
-    TIM2_COMPARE_BIT_0 = 1U;
-    TIM2_COMPARE_BIT_1 = 3U;
-  }    
-}
+/* WS2812 Prescaler Value */
+#define WS2812_TIM2_PSC 0
+#define WS2812_TIM2_ARR 19U
+#define WS2812_TIM2_COMPARE_BIT_0 6U
+#define WS2812_TIM2_COMPARE_BIT_1 12U
 
 
-void ws2812_init(WS2812_CHANNEL_PIN pin) {    
+void ws2812_init(WS2812_CHANNEL_PIN pin) {     
   gpio_out_push_pull_fast_mode(GPIO_D, pin);
   clk_peripheral_1_clock_enable(CLK_TIM2);  
   tim2_auto_reload_preload_enable();
@@ -59,109 +44,130 @@ void ws2812_init(WS2812_CHANNEL_PIN pin) {
     tim2_output_compare3_mode_set(TIM2_OC_PWM_MODE_1);
     tim2_cc3_preload_enable();
     tim2_cc3_enable();
-  }
+  }  
 
-  ws2812_timing_calibrate();
+  time_init();
 }
 
 
-void ws2812_write_frame_ch1(uint8_t *frame, uint8_t frame_len) {
-  tim2_prescaler_set(WS2812_TIM2_PSC);
-  tim2_auto_reload_write(TIM2_ARR);          
-  tim2_cc1_write(((frame[0] >> 7) & 1) ? TIM2_COMPARE_BIT_1 : TIM2_COMPARE_BIT_0);                  
-  tim2_update_event_generate();
-  tim2_update_irq_flag_clear();
-  tim2_cc1_irq_flag_clear();
-  uint8_t first_entry = 1;
-  
-  for(uint8_t i = 0; i < frame_len; i++) {
-    for(int j = 7; j >= 0; j--) {                  
-      if(first_entry) {
-        tim2_counter_enable();                    
-        first_entry = 0;
-        continue;        
-      }
+void ws2812_write_frame_ch1(uint8_t *frame, uint8_t frame_len) {  
+  global_interrupt_disable();
 
-      
+  CLK_MASTER_SRC current_clock_src = clk_master_get_source();
+  CPU_DIV_PRESCALAR current_cpu_divider = clk_cpu_div_prescalar_read();
+  HSI_DIV_PRESCALAR current_hsi_divider = clk_hsi_div_prescalar_read();
+
+  clk_fmaster_switch_src_auto_mode(CLK_MASTER_SRC_HSI);
+  clk_hsi_and_cpu_div_prescalar_set(CLK_HSI_DIV_1, CLK_CPU_DIV_1);
+
+  tim2_prescaler_set(WS2812_TIM2_PSC);
+  tim2_auto_reload_write(WS2812_TIM2_ARR);            
+  uint8_t cc1_value = (frame[0] & 0x80) ? WS2812_TIM2_COMPARE_BIT_1 : WS2812_TIM2_COMPARE_BIT_0;
+  tim2_cc1_write(cc1_value);
+  tim2_update_event_generate();        
+  tim2_cc1_irq_flag_clear();
+  tim2_counter_enable();                    
+
+  for(uint8_t i = 0; i < frame_len; i++) {                      
+    uint8_t frame_byte = frame[i];
+    for(uint8_t mask = (i == 0) ? 0x40 : 0x80; mask > 0; mask >>= 1) {
+      cc1_value = (frame_byte & mask) ? WS2812_TIM2_COMPARE_BIT_1 : WS2812_TIM2_COMPARE_BIT_0;
       while(!tim2_cc1_irq_flag_read());
       tim2_cc1_irq_flag_clear();
-      tim2_cc1_write(((frame[i] >> j) & 1) ? TIM2_COMPARE_BIT_1 : TIM2_COMPARE_BIT_0);                              
-
-      while(!tim2_update_irq_flag_read());  
-      tim2_update_irq_flag_clear();
+      tim2_cc1_write(cc1_value);                              
     }              
   }         
 
   while(!tim2_cc1_irq_flag_read());
-  tim2_cc1_irq_flag_clear();
   tim2_counter_disable();
-  time_delay_us(300);    
+  tim2_cc1_irq_flag_clear();
+  tim2_update_irq_flag_clear();
+    
+  time_delay_us(300U);    
+  
+  clk_fmaster_switch_src_auto_mode(current_clock_src);
+  clk_hsi_and_cpu_div_prescalar_set(current_hsi_divider, current_cpu_divider);
+  global_interrupt_enable();
 }
 
 
 void ws2812_write_frame_ch2(uint8_t *frame, uint8_t frame_len) {
-  tim2_prescaler_set(WS2812_TIM2_PSC);
-  tim2_auto_reload_write(TIM2_ARR);          
-  tim2_cc2_write(((frame[0] >> 7) & 1) ? TIM2_COMPARE_BIT_1 : TIM2_COMPARE_BIT_0);                  
-  tim2_update_event_generate();
-  tim2_update_irq_flag_clear();
-  tim2_cc2_irq_flag_clear();
-  uint8_t first_entry = 1;
-  
-  for(uint8_t i = 0; i < frame_len; i++) {
-    for(int j = 7; j >= 0; j--) {                  
-      if(first_entry) {
-        tim2_counter_enable();                    
-        first_entry = 0;
-        continue;        
-      }
+  global_interrupt_disable();
 
-      
+  CLK_MASTER_SRC current_clock_src = clk_master_get_source();
+  CPU_DIV_PRESCALAR current_cpu_divider = clk_cpu_div_prescalar_read();
+  HSI_DIV_PRESCALAR current_hsi_divider = clk_hsi_div_prescalar_read();
+
+  clk_fmaster_switch_src_auto_mode(CLK_MASTER_SRC_HSI);
+  clk_hsi_and_cpu_div_prescalar_set(CLK_HSI_DIV_1, CLK_CPU_DIV_1);
+
+  tim2_prescaler_set(WS2812_TIM2_PSC);
+  tim2_auto_reload_write(WS2812_TIM2_ARR);            
+  uint8_t cc2_value = (frame[0] & 0x80) ? WS2812_TIM2_COMPARE_BIT_1 : WS2812_TIM2_COMPARE_BIT_0;
+  tim2_cc2_write(cc2_value);
+  tim2_update_event_generate();        
+  tim2_cc2_irq_flag_clear();
+  tim2_counter_enable();                    
+
+  for(uint8_t i = 0; i < frame_len; i++) {                      
+    uint8_t frame_byte = frame[i];
+    for(uint8_t mask = (i == 0) ? 0x40 : 0x80; mask > 0; mask >>= 1) {
+      cc2_value = (frame_byte & mask) ? WS2812_TIM2_COMPARE_BIT_1 : WS2812_TIM2_COMPARE_BIT_0;
       while(!tim2_cc2_irq_flag_read());
       tim2_cc2_irq_flag_clear();
-      tim2_cc2_write(((frame[i] >> j) & 1) ? TIM2_COMPARE_BIT_1 : TIM2_COMPARE_BIT_0);                              
-
-      while(!tim2_update_irq_flag_read());  
-      tim2_update_irq_flag_clear();
+      tim2_cc2_write(cc2_value);                              
     }              
   }         
 
   while(!tim2_cc2_irq_flag_read());
-  tim2_cc2_irq_flag_clear();
   tim2_counter_disable();
-  time_delay_us(300);    
+  tim2_cc2_irq_flag_clear();
+  tim2_update_irq_flag_clear();
+  
+  time_delay_us(300U);    
+  
+  clk_fmaster_switch_src_auto_mode(current_clock_src);
+  clk_hsi_and_cpu_div_prescalar_set(current_hsi_divider, current_cpu_divider);
+  global_interrupt_enable();
 }
 
 
 void ws2812_write_frame_ch3(uint8_t *frame, uint8_t frame_len) {
-  tim2_prescaler_set(WS2812_TIM2_PSC);
-  tim2_auto_reload_write(TIM2_ARR);          
-  tim2_cc3_write(((frame[0] >> 7) & 1) ? TIM2_COMPARE_BIT_1 : TIM2_COMPARE_BIT_0);                  
-  tim2_update_event_generate();
-  tim2_update_irq_flag_clear();
-  tim2_cc3_irq_flag_clear();
-  uint8_t first_entry = 1;
-  
-  for(uint8_t i = 0; i < frame_len; i++) {
-    for(int j = 7; j >= 0; j--) {                  
-      if(first_entry) {
-        tim2_counter_enable();                    
-        first_entry = 0;
-        continue;        
-      }
+  global_interrupt_disable();
 
-      
+  CLK_MASTER_SRC current_clock_src = clk_master_get_source();
+  CPU_DIV_PRESCALAR current_cpu_divider = clk_cpu_div_prescalar_read();
+  HSI_DIV_PRESCALAR current_hsi_divider = clk_hsi_div_prescalar_read();
+
+  clk_fmaster_switch_src_auto_mode(CLK_MASTER_SRC_HSI);
+  clk_hsi_and_cpu_div_prescalar_set(CLK_HSI_DIV_1, CLK_CPU_DIV_1);
+
+  tim2_prescaler_set(WS2812_TIM2_PSC);
+  tim2_auto_reload_write(WS2812_TIM2_ARR);            
+  uint8_t cc3_value = (frame[0] & 0x80) ? WS2812_TIM2_COMPARE_BIT_1 : WS2812_TIM2_COMPARE_BIT_0;
+  tim2_cc3_write(cc3_value);
+  tim2_update_event_generate();        
+  tim2_cc3_irq_flag_clear();
+  tim2_counter_enable();                    
+
+  for(uint8_t i = 0; i < frame_len; i++) {                      
+    uint8_t frame_byte = frame[i];
+    for(uint8_t mask = (i == 0) ? 0x40 : 0x80; mask > 0; mask >>= 1) {
+      cc3_value = (frame_byte & mask) ? WS2812_TIM2_COMPARE_BIT_1 : WS2812_TIM2_COMPARE_BIT_0;
       while(!tim2_cc3_irq_flag_read());
       tim2_cc3_irq_flag_clear();
-      tim2_cc3_write(((frame[i] >> j) & 1) ? TIM2_COMPARE_BIT_1 : TIM2_COMPARE_BIT_0);                              
-
-      while(!tim2_update_irq_flag_read());  
-      tim2_update_irq_flag_clear();
+      tim2_cc3_write(cc3_value);                              
     }              
   }         
 
   while(!tim2_cc3_irq_flag_read());
-  tim2_cc3_irq_flag_clear();
   tim2_counter_disable();
-  time_delay_us(300);    
+  tim2_cc3_irq_flag_clear();
+  tim2_update_irq_flag_clear();
+
+  time_delay_us(300U);    
+  
+  clk_fmaster_switch_src_auto_mode(current_clock_src);
+  clk_hsi_and_cpu_div_prescalar_set(current_hsi_divider, current_cpu_divider);
+  global_interrupt_enable();
 }
--- STM8S103F3_L1_Drivers/ws2812.h
+++ STM8S103F3_L1_Drivers/ws2812.h
@@ -16,17 +16,6 @@ typedef enum {
   WS2812_CH3_PD2 = 2
 } WS2812_CHANNEL_PIN;
 
-
-/* Global Variable and Constant Declarations */
-extern uint8_t TIM2_ARR;
-extern uint8_t TIM2_COMPARE_BIT_0;
-extern uint8_t TIM2_COMPARE_BIT_1;
-
-
-/* Timing Parameters Calibration Function */
-void ws2812_timing_calibrate(void);
-
-
 /* WS2812 Initialization Function */
 void ws2812_init(WS2812_CHANNEL_PIN pin);
 
